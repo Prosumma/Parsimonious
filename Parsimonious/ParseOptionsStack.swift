25d73600f4df4df19f62a6b139282608efb82a13
Gregory Higley <code@revolucent.net>
2015-07-03 14:08:46 -0400

    OS X

---
+++ Parsimonious/ParseOptionsStack.swift
@@ -0,0 +1,9 @@
+//
+//  ParseOptionsStack.swift
+//  Parsimonious
+//
+//  Created by Gregory Higley on 7/3/15.
+//  Copyright © 2015 Prosumma LLC. All rights reserved.
+//
+
+import Foundation
--- Parsimonious/Parsimonious.h
+++ Parsimonious/Parsimonious.h
@@ -6,7 +6,7 @@
 //  Copyright © 2015 Prosumma LLC. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for Parsimonious.
 FOUNDATION_EXPORT double ParsimoniousVersionNumber;
