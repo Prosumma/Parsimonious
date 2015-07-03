53d0cd3c7f1f04b0334ef08c33a640e7b217ad06
Gregory Higley <code@revolucent.net>
2015-07-03 13:59:23 -0400

    Initial commit

---
+++ Parsimonious/Parsimonious.h
@@ -0,0 +1,19 @@
+//
+//  Parsimonious.h
+//  Parsimonious
+//
+//  Created by Gregory Higley on 7/3/15.
+//  Copyright © 2015 Prosumma LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Parsimonious.
+FOUNDATION_EXPORT double ParsimoniousVersionNumber;
+
+//! Project version string for Parsimonious.
+FOUNDATION_EXPORT const unsigned char ParsimoniousVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Parsimonious/PublicHeader.h>
+
+
