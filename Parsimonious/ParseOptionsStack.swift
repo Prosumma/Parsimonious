88d6ced9f0d1bb00ae2dc272561c6d04d3ce05b0
Gregory Higley <code@revolucent.net>
2015-07-03 14:26:20 -0400

    ParseOptions

--- Parsimonious/ParseOptionsStack.swift
+++ Parsimonious/ParseOptionsStack.swift
@@ -8,12 +8,41 @@
 
 import Foundation
 
+public struct ParseOptions {
+    let skipCharacters: Optional<NSCharacterSet>?
+    let caseInsensitive: Bool?
+}
+
+public struct RealizedParseOptions {
+    let skipCharacters: NSCharacterSet?
+    let caseInsensitive: Bool
+}
+
 class ParseOptionsStack {
     
-    private struct RealParseOptions {
-        
+    private var stack = [RealizedParseOptions]()
+    
+    var current: RealizedParseOptions {
+        guard let options = stack.first else {
+            return RealizedParseOptions(skipCharacters: nil, caseInsensitive: false)
+        }
+        return options
     }
     
+    func push(options: ParseOptions) {
+        var skipCharacters = current.skipCharacters
+        if let shouldSkipCharacters = options.skipCharacters {
+            switch shouldSkipCharacters {
+            case .Some(let characterSet): skipCharacters = characterSet
+            case .None: skipCharacters = nil
+            }
+        }
+        let realizedOptions = RealizedParseOptions(skipCharacters: skipCharacters, caseInsensitive: options.caseInsensitive ?? current.caseInsensitive)
+        stack.append(realizedOptions)
+    }
     
+    func pop() {
+        stack.removeLast()
+    }
     
 }
